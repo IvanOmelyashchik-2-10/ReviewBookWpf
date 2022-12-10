0f733c481022ede0299cb4ad3773b912e6e7ec55
Иван Омелящик <cpt.legion.337@gmail.com>
2022-12-10 17:42:28 +0300

    Определение класс отдельной командой

---
+++ ReviewBookWpf/Infastructures/Commands/CloseAppCommand.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ReviewBookWpf.Infastructures.Commands
+{
+    internal class CloseAppCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter) => Application.Current.Shutdown();
+
+    }
+}
