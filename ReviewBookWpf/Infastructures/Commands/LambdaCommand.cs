19ecb37ccdefb6bfa37d6f162dda01d0419d75a4
Иван Омелящик <cpt.legion.337@gmail.com>
2022-12-10 17:09:42 +0300

    Главный клаас для нашего приложения

---
+++ ReviewBookWpf/Infastructures/Commands/LambdaCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ReviewBookWpf.Infastructures.Commands
+{
+    internal class LambdaCommand : Command
+    {
+        private readonly Action<object> _Execute;
+        private readonly Func<object,bool> _CanExecute;
+        public LambdaCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            _Execute = execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = canExecute;
+        }
+
+        public override void Execute(object parameter) => _Execute(parameter);
+
+
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+
+    }
+}
