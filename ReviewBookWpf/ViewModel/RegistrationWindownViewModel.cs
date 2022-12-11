6189394e9b3d1da1bb5af9adc186defd29e8c6bd
Иван Омелящик <cpt.legion.337@gmail.com>
2022-12-11 18:20:59 +0300

    Разработка и добавления окно регистрации

---
+++ ReviewBookWpf/ViewModel/RegistrationWindownViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReviewBookWpf.ViewModel.Base;
+
+namespace ReviewBookWpf.ViewModel
+{
+    internal class RegistrationWindownViewModel : ViewModels
+    {
+    }
+}
